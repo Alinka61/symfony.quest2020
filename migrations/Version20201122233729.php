6e9ce5df2c0119d6ac23204265f73473c8633ac9
alpha1fly <ru101.fly@gmail.com>
2020-11-23 13:52:12 +0300

    auth1

--- migrations/Version20201122233729.php
+++ migrations/Version20201122233729.php
@@ -20,14 +20,12 @@ final class Version20201122233729 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE vacancy (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, annotation VARCHAR(1022) NOT NULL, link VARCHAR(255) DEFAULT NULL, tags VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, email VARCHAR(255) NOT NULL, img VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE product CHANGE description description VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE vacancy (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, annotation VARCHAR(1022) NOT NULL, link VARCHAR(255) DEFAULT NULL, tags VARCHAR(255) NOT NULL, description LONGTEXT, email VARCHAR(255) NOT NULL, img VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE vacancy');
         $this->addSql('ALTER TABLE product CHANGE description description VARCHAR(1021) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
