dce6796fc652f7c3dcc2434f2be0465b23e53ac7
alpha1fly <ru101.fly@gmail.com>
2020-11-24 15:28:07 +0300

    main_user_update_v2.0

--- src/Controller/UserMainController.php
+++ src/Controller/UserMainController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use App\Entity\Vacancy;
+use App\Form\VacancyType;
+use App\Repository\VacancyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +17,14 @@ class UserMainController extends AbstractController
     /**
      * @Route("/user/main", name="user_main")
      */
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, VacancyRepository $vacancyRepository): Response
     {
-        $products = $productRepository->findAll();
+        $products  = $productRepository->findAll();
+        $vacancies = $vacancyRepository->findAll();
         return $this->render('user_main/index.html.twig', [
             'controller_name' => 'UserMainController',
-            'products' => $products
+            'products' => $products,
+            'vacancies' => $vacancies
         ]);
     }
 }
